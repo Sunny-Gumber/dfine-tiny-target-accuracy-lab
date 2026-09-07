991afdaf0560f89a71f2e95d20d13aff406557a5
Sunny-Gumber <68242840+Sunny-Gumber@users.noreply.github.com>
2026-09-07 10:54:04 +0530

    Add browser face detection crop lab
    * Add face detection test lab
    
    * Route face test lab by query parameter
    
    * Style face detection test lab
    
    * Load face lab styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import FaceLab from "./FaceLab";
 import "./styles.css";
+import "./face.css";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("Root element not found");
 
+const params = new URLSearchParams(window.location.search);
+const page = params.get("lab") === "face" ? <FaceLab /> : <App />;
+
 createRoot(root).render(
   <StrictMode>
-    <App />
+    {page}
   </StrictMode>,
 );
