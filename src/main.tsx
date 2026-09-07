1da69f608a6a78f9381c47f31c4b54a6a25b76b2
Sunny-Gumber <68242840+Sunny-Gumber@users.noreply.github.com>
2026-09-07 12:00:13 +0530

    Route face lab to stable age range pipeline

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import FaceLabBundled from "./FaceLabBundled";
 import FaceLabScrfd from "./FaceLabScrfd";
+import FaceLabStableAge from "./FaceLabStableAge";
 import "./styles.css";
 import "./face.css";
 
@@ -11,7 +12,8 @@ if (!root) throw new Error("Root element not found");
 
 const params = new URLSearchParams(window.location.search);
 const isFaceLab = params.get("lab") === "face";
-const facePage = params.get("engine") === "blaze" ? <FaceLabBundled /> : <FaceLabScrfd />;
+const engine = params.get("engine");
+const facePage = engine === "blaze" ? <FaceLabBundled /> : engine === "single-age" ? <FaceLabScrfd /> : <FaceLabStableAge />;
 const page = isFaceLab ? facePage : <App />;
 
 createRoot(root).render(
