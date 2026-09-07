1c329a8f738c7640f5eecb9e3faca7d3b894b099
Sunny-Gumber <68242840+Sunny-Gumber@users.noreply.github.com>
2026-09-07 11:00:47 +0530

    Route face lab to bundled detector

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import FaceLab from "./FaceLab";
+import FaceLab from "./FaceLabBundled";
 import "./styles.css";
 import "./face.css";
 
