fd0c776b37aa968d2cd914cf4a5bd58627d8e503
Sunny-Gumber <68242840+Sunny-Gumber@users.noreply.github.com>
2026-09-22 06:18:54 +0530

    Use lifecycle runtime compatible with Android 16 SDK

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
     androidTestImplementation(composeBom)
 
     implementation("androidx.activity:activity-compose:1.13.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.11.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
