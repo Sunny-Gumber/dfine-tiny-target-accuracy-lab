2b84d0539e8e73fd19e166073295cc6588667c6e
Sunny-Gumber <68242840+Sunny-Gumber@users.noreply.github.com>
2026-09-22 06:36:09 +0530

    Connect native AI ViewModel to Compose activity

--- android/app/src/main/java/com/sunnygumber/cctvaivisionlab/MainActivity.kt
+++ android/app/src/main/java/com/sunnygumber/cctvaivisionlab/MainActivity.kt
@@ -3,15 +3,18 @@ package com.sunnygumber.cctvaivisionlab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.material3.MaterialTheme
 import com.sunnygumber.cctvaivisionlab.ui.CctvAiApp
+import com.sunnygumber.cctvaivisionlab.ui.VisionViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                CctvAiApp()
+                val visionViewModel: VisionViewModel = viewModel()
+                CctvAiApp(visionViewModel)
             }
         }
     }
